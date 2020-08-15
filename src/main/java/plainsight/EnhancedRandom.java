a5cc4ce9b52745eecd9a2120bcd356e182d40b1b
= <=>
2020-08-15 12:39:30 -0400

    Update EnhancedRandom.java

--- src/main/java/plainsight/EnhancedRandom.java
+++ src/main/java/plainsight/EnhancedRandom.java
@@ -40,16 +40,12 @@ public class EnhancedRandom {
     }
     
     protected int[] generateShuffledIntArray(int size, int exceptionIndex) {
-        int[] array = new int[size];
+        int[] array = new int[size - 1];
         System.out.println(System.currentTimeMillis());
         
         IntStream.range(0, size - 1).parallel().forEach(i -> {
             array[i] = i;
         });
-        
-        //for (int i = 0; i < size; i++) {
-        //    
-        //}
         
         if (exceptionIndex != size - 1) {
             array[exceptionIndex] = size - 1;
